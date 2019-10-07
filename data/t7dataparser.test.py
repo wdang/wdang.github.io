c2428a28aad4e140053eb963ef55a9d7183eaae4
Will Dang <williamvandang@gmail.com>
2019-10-06 22:07:31 -0700

    added unittest

---
+++ data/t7dataparser.test.py
@@ -0,0 +1,11 @@
+import unittest
+import t7dataparser
+
+class TestParser(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    s = "hello, my name is ¢arl... ¡Hola!"
+    print(s)    
+    unittest.main()
